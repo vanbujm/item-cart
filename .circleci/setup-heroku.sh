c8ae942e88097dff03253183cf1eeffff4112f67
Jonathan Van Buren <jvburen@thoughtworks.com>
2017-10-11 15:53:29 +0800

    changing to workflows

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/react-rails-nopsema.git
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 machine git.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
-# Add heroku.com to the list of known hosts
-ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+heroku git:remote -a $HEROKU_APP
