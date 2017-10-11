e06d10f771a75f351f725c2127a8dfaaa0909727
Jonathan Van Buren <jvburen@thoughtworks.com>
2017-10-11 16:45:56 +0800

    finishing workflow

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -6,9 +6,6 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
-machine git.heroku.com
-  login $HEROKU_LOGIN
-  password $HEROKU_API_KEY
 EOF
 
 heroku git:remote -a $HEROKU_APP
