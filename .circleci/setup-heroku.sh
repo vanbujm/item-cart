6a05aa13e98b5764cb8a43bb895760f9c449ae7b
Jonathan Van Buren <jvburen@thoughtworks.com>
2017-10-11 16:54:56 +0800

    finishing workflow

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+git remote add heroku https://git.heroku.com/react-rails-nopsema.git
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
@@ -8,4 +12,5 @@ machine api.heroku.com
   password $HEROKU_API_KEY
 EOF
 
-heroku git:remote -a $HEROKU_APP
+git add public
+git commit -m "Precompile assets"
