76e1d7bb3cb2be54d7cf1345f375f40c6085573e
Jonathan Van Buren <jvburen@thoughtworks.com>
2017-10-11 14:44:15 +0800

    Revert "changed component"
    This reverts commit be181a0ae1579d3568bcbdab31018b13466eb997.

--- app/javascript/components/SampleComponent.js
+++ app/javascript/components/SampleComponent.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class SampleComponent extends Component {
     render() {
         return (
-            <h1>Hi, I'm SampleComponent! HOT RELOAD IS THE BEST</h1>
+            <h1>Hi, I'm SampleComponent!</h1>
         );
     }
 }
